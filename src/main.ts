69e36d243c6116c3c4b2604645c35fcc00852030
lvneto <lauro.neto.1995@gmail.com>
2023-06-23 16:08:11 -0300

    install swagger and changing show swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('main');
+
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  if (process.env.NODE_ENV === 'development') {
+    logger.log(`Initializing Swagger module for ${process.env.NODE_ENV}`);
+
+    const config = new DocumentBuilder()
+      .setTitle('Zenbit-Challenger')
+      .setDescription('Official documentation for platform APIs')
+      .setVersion('0.1')
+      .addBearerAuth({ in: 'header', type: 'http' })
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    });
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: 'https://url.production',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    });
+  }
+
+  await app.listen(process.env.PORT || 3333);
 }
+
 bootstrap();
